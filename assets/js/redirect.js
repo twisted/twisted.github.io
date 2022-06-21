dd9413a026da2d7cc4023faff21ed8a992b27e1c
Cristina Marinescu <icmarinescu23@gmail.com>
2022-06-21 15:35:10 +0300

    [#11] Add 404 page.

---
+++ assets/js/redirect.js
@@ -0,0 +1,22 @@
+/*
+404 page
+Show link to redirect to correct page.
+*/
+
+// JS is enbled
+$('#js-enable-js').addClass('tw-hidden')
+// Show link to page
+$('#js-redirection').removeClass('tw-hidden')
+
+var p = window.location.pathname
+var new_url = 'https://github.com/twisted/twisted-trac-migration-3/issues/'
+if (p.match('/trac/ticket/')) {
+    p = window.location.pathname
+    var r = p.split('/')
+    var t = r[r.length - 1]
+    if(t == '') {
+        t = r[r.length - 2]
+    }
+    new_url = 'https://github.com/twisted/twisted-trac-migration-3/issues/' + t
+    $('#link').attr("href", new_url)
+}
