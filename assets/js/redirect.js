789a78533a5f72df8ccd29aa1e1504b7976c0f1a
Cristina Marinescu <icmarinescu23@gmail.com>
2022-07-01 14:14:09 +0300

    add rules

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -2,12 +2,24 @@
 404 page
 Show link to redirect to correct page and redirect.
 */
+import {to_homepage} from "./redirect_rules.mjs"
+import {rules} from "./redirect_rules.mjs"
 import {migrated_tickets} from "./redirect_rules.mjs"
 
-var p = window.location.pathname
+const github = 'https://github.com/twisted/twisted'
+const github_issues = github + '/issues/'
 
-if (p.match('/trac/ticket/')) {
-    var new_url = 'https://github.com/twisted/twisted/issues/'
+var path = window.location.pathname
+var path_simple = stripTrailingSlash(path)
+
+for (p in to_homepage) {
+    if (path_simple.match(p)){
+        window.location = '/'
+    }
+}
+
+if (path.match('/trac/ticket/')) {
+    var new_url = github_issues
     p = window.location.pathname
     var r = p.split('/')
     var trac_id = r[r.length - 1]
@@ -33,5 +45,12 @@ function trac_to_github(trac_id) {
     if (migrated_tickets.has(trac_id)) {
         return migrated_tickets.get(trac_id)
     }
-    return ''
+    return false
+}
+
+function stripTrailingSlash(str) {
+    if(str.substr(-1) === '/') {
+        return str.substr(0, str.length - 1);
+    }
+    return str;
 }
